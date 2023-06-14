8aca71adc9437bbdc01afa6bf9333d894654bc1a
Monclau1260 <132160439+Monclau1260@users.noreply.github.com>
2023-06-13 22:42:29 -0400

    delete
    delete

---
+++ deleteme.py
@@ -0,0 +1,14 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import numpy
+>>> import matplot
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    import matplot
+ModuleNotFoundError: No module named 'matplot'
+>>> print"anything"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+>>> print ("anything")
+anything
+>>> 5+7
+12
